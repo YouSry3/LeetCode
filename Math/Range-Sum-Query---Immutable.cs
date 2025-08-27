934702d3f0004157b100f7c5536c98a175794c55
YouSry3 <131623906+YouSry3@users.noreply.github.com>
2025-08-27 16:41:39 +0300

    [303] [Time Beats: 32.46%] [Memory Beats: 52.49%] - LeetPush

---
+++ Math/Range-Sum-Query---Immutable.cs
@@ -0,0 +1,22 @@
+public class NumArray {
+    public int[] Nums;
+
+    public NumArray(int[] nums) {
+        Nums = nums;
+    }
+    
+    public int SumRange(int left, int right) {
+        int sum=0;
+        for(int i=left; i<=right; i++){
+            sum += Nums[i];
+
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
